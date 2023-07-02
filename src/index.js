ed3a206d41e33f868b76980cf0258579c393908b
petosagie <petosagie@gmail.com>
2023-07-03 00:06:58 +0100

    setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// To make a movie display website
+
+const hellonNode = document.createElement('h1');
+hellonNode.textContent = 'Hello World!';
+document.getElementById('root').appendChild(hellonNode);
