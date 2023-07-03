82b6c4997ae6124c67c6c3f469b616d7e2381510
petosagie <petosagie@gmail.com>
2023-07-03 11:44:55 +0100

    js files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// To make a movie display website
-
-const hellonNode = document.createElement('h1');
-hellonNode.textContent = 'Hello World!';
-document.getElementById('root').appendChild(hellonNode);
---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+// To make a movie display website
