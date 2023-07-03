719d39da0e5a24cbaaf502dcde349920c45875e8
petosagie <petosagie@gmail.com>
2023-07-03 13:03:15 +0100

    index.html

--- src/js/moviePage.js
+++ src/js/moviePage.js
@@ -13,7 +13,7 @@ const movieGenres = await movieData.getGenres()
 
 // Set the title of the page
 const title = document.querySelector('title')
-title.innerText = `Netflax | ${movie.title}`
+title.innerText = `Moviesdix | ${movie.title}`
 
 // Set the main header
 const heading = document.querySelector('.title-heading')
